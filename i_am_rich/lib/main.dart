01f2c843d5fe701ab3c93d48ed39d31f66edcf02
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-02 13:24:17 +0900

    feat: i_am_rich - Image 파일 추가

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I am Rich'),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://i1.wp.com/tripplus.co.kr/wp-content/uploads/2021/02/blog_the_trip_222249979391_1.jpg?w=696&ssl=1'),
+          ),
         ),
       ),
     ),
