b7c1e3bd13fd06d0d3d15fabe6daf63b7d97df4c
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-03 18:15:49 +0900

    feat: mi_card_flutter 초기 파일 생성

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.only(left: 30.0),
+              padding: EdgeInsets.all(20.0),
+              color: Colors.white,
+              child: Text("Hello"),
+            ),
+        ),
+      ),
+    );
+  }
+}
