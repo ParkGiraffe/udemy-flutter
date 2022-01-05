adc4e7e8eca69b2a0c0e8ed929102265ac0e5d2c
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-05 14:29:49 +0900

    feat: dicee - 주사위 이미지에 TextButton()위젯을 적용

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -23,14 +23,24 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Left Button got pressed.');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Right Button got pressed.');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
         ],
