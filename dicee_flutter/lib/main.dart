9610bbeb3000647d455e6e7f09ee223db12934f0
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-06 14:57:42 +0900

    feat: dicee - rightDiceNumber 변수 생성, math library를 이용하여 임의의 수 생성 기능 추가

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   return runApp(
@@ -23,6 +25,7 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
 
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +38,7 @@ class _DicePageState extends State<DicePage> {
               child: TextButton(
                 onPressed: () {
                   setState(() {
-                    leftDiceNumber = 4;
+                    leftDiceNumber = Random().nextInt(6) + 1;
                   });
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -47,9 +50,11 @@ class _DicePageState extends State<DicePage> {
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
                 onPressed: () {
-                  print('Right Button got pressed.');
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
                 },
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ),
