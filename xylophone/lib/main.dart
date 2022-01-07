d8fc14eb707dc7c759f61703aef1e1efdf8f7a32
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-07 15:57:55 +0900

    feat: xylophone - playSound, buildKey 함수 생성

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,19 +4,44 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Widget buildKey({int soundNumber = 1, Color colorName = Colors.red}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: TextButton.styleFrom(
+          primary: colorName,
+          backgroundColor: colorName,
+        ),
+        child: Text("$soundNumber"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text("note1"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNumber: 1, colorName: Colors.red),
+              buildKey(soundNumber: 2, colorName: Colors.orange),
+              buildKey(soundNumber: 3, colorName: Colors.yellow),
+              buildKey(soundNumber: 4, colorName: Colors.green),
+              buildKey(soundNumber: 5, colorName: Colors.teal),
+              buildKey(soundNumber: 6, colorName: Colors.blue),
+              buildKey(soundNumber: 7, colorName: Colors.purple,)
+            ],
           ),
         ),
       ),
