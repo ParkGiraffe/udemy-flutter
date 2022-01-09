9599ec90de232226a33fb6320baadabcd0cf2888
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-09 18:57:23 +0900

    feat: bmi_calculator - 테마 적용

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -2,11 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
 
+final theme1 = ThemeData(
+  scaffoldBackgroundColor: Color(0xFF0A0E21),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      color: Colors.white,
+    ),
+    bodyText2: TextStyle(
+      color: Colors.white,
+    ),
+  ),
+  colorScheme: ThemeData().colorScheme.copyWith(
+        primary: Color(0xFF0A0E21),
+        secondary: Colors.purple,
+      ),
+);
+
+final theme2 = ThemeData.dark().copyWith(
+  // primaryColor: Color(0xFF0A0E21),
+  scaffoldBackgroundColor: Color(0xFF0A0E21),
+  colorScheme: ColorScheme.light().copyWith(
+    secondary: Colors.purple,
+    primary: Color(0xFF0A0E21),
+  ),
+);
+
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      theme: theme2,
     );
   }
 }
@@ -25,9 +51,6 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
