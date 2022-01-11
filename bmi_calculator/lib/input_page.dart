2145de09c322f6442c75628243c5831a75bace57
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-11 14:58:14 +0900

    feat: bmi_calculator - ReusableCard에 onPress Function 인자를 받도록 설정

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -33,38 +33,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == Gender.male
-                          ? activeCardColour
-                          : inactiveCardColour,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.mars,
-                        label: "MALE",
-                      ),
+                    colour: selectedGender == Gender.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
                     ),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == Gender.female
-                          ? activeCardColour
-                          : inactiveCardColour,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.venus,
-                        label: "FEMALE",
-                      ),
+                    colour: selectedGender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
                     ),
                   ),
                 ),
--- bmi_calculator/lib/reusable_card.dart
+++ bmi_calculator/lib/reusable_card.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  // const ReusableCard({
-  //   Key? key,
-  // }) : super(key: key);
+  const ReusableCard({
+    Key? key,
+    required this.colour,
+    this.cardChild,
+    this.onPress,
+  }) : super(key: key);
 
-  Color colour;
-  Widget? cardChild;
+  final Color colour;
+  final Widget? cardChild;
+  final void Function()? onPress;
 
-  ReusableCard({required this.colour, this.cardChild});
+  //ReusableCard({required this.colour, this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
+        margin: EdgeInsets.all(15.0),
+        child: cardChild,
       ),
-      margin: EdgeInsets.all(15.0),
-      child: cardChild,
     );
   }
 }
