094544116c033aaab6d127c735145f8e5c44d06a
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-16 12:50:28 +0900

    feat: clima - http package를 이용해서 getData()함수 생성

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/utilities/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +14,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation(); // await를 쓸려면 반환형이 Future여야 한다.
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get('url');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
