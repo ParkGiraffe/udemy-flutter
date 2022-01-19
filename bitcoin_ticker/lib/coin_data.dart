5ed53594323aea3df8003e929bf2fdb5da482ffc
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-19 15:17:42 +0900

    feat: bitcoin_tracker - api key 추가

--- bitcoin_ticker/lib/coin_data.dart
+++ bitcoin_ticker/lib/coin_data.dart
@@ -29,7 +29,7 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '87148869-4D95-4848-89AF-DAB381E1F092';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
