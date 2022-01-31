cd4125c2fc457bfcee0f4cd5266475032a43b4ce
ParkGiraffe <op5321@soongsil.ac.kr>
2022-01-31 15:08:45 +0900

    feat: todoey - ListView와 내부 요소 디자인틀 구현

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +17,20 @@ class TasksScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: EdgeInsets.only(top: 60.0, left:30.0, right:30.0, bottom:30.0),
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CircleAvatar(
                   backgroundColor: Colors.white,
                   radius: 30.0,
-                  child: Icon(
-                    Icons.list,
-                    color: Colors.lightBlueAccent,
-                    size: 45.0
-                  ),
+                  child: Icon(Icons.list,
+                      color: Colors.lightBlueAccent, size: 45.0),
                 ),
-                SizedBox(height: 20.0,),
+                SizedBox(
+                  height: 20.0,
+                ),
                 Text(
                   'Todoey',
                   style: TextStyle(
@@ -50,16 +49,40 @@ class TasksScreen extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(height: 15.0,),
-          Expanded(child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20.0),
-                topLeft: Radius.circular(20.0),
+          SizedBox(
+            height: 15.0,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  topLeft: Radius.circular(20.0),
+                ),
+              ),
+              child: ListView(
+                padding: EdgeInsets.only(top: 60.0, right: 80.0, left: 80.0),
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'list1',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Icon(
+                        Icons.check_box_outline_blank,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ),),
+          ),
         ],
       ),
     );
