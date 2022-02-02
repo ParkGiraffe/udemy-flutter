a11fd3932738ee0bc21e7e7c560de655cc46d223
ParkGiraffe <op5321@soongsil.ac.kr>
2022-02-02 19:16:52 +0900

    feat: todoey - borderRadius 적용

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/screens/add_task_screen.dart';
 import 'package:todoey/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -12,7 +13,7 @@ class TasksScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.lightBlueAccent,
         onPressed: () {
-          showModalBottomSheet(context: context, builder: (context) => Container());
+          showModalBottomSheet(context: context, builder: (context) => AddTaskScreen());
         },
       ),
       backgroundColor: Colors.lightBlueAccent,
