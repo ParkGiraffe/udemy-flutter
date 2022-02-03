f8286aed4283dac4d11e531491a59260cddb833d
ParkGiraffe <op5321@soongsil.ac.kr>
2022-02-03 17:30:32 +0900

    feat: todoey - checkbox를 누르면 중간선이 생기도록 구현

--- todoey/lib/widgets/task_tile.dart
+++ todoey/lib/widgets/task_tile.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/material.dart';
 
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
 
-class TaskTile extends StatelessWidget {
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: TaskCheckBox(),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState!;
+          });
+        },
+      ),
     );
   }
 }
 
-class TaskCheckBox extends StatefulWidget {
-  const TaskCheckBox({
-    Key? key,
-  }) : super(key: key);
+class TaskCheckBox extends StatelessWidget {
+  final bool? checkboxState;
+  late Function(bool?)? toggleCheckboxState;
 
-  @override
-  State<TaskCheckBox> createState() => _TaskCheckBoxState();
-}
-
-class _TaskCheckBoxState extends State<TaskCheckBox> {
-
-  bool? isChecked = true;
+  TaskCheckBox(
+      {required this.checkboxState, required this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue;
-        });
-      },
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
