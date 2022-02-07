6c41758d3e5d6892e1b2e7d329d1444fe4e491c2
ParkGiraffe <op5321@soongsil.ac.kr>
2022-02-07 14:59:03 +0900

    feat: todoey - TasksList를 stless로 변경

--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -1,39 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
-import 'package:todoey/model/task.dart';
 import 'package:todoey/model/task_data.dart';
 import 'package:todoey/widgets/task_tile.dart';
 
-class TasksList extends StatefulWidget {
-  //List<Task> tasks;
-  //TasksList(this.tasks);
-  @override
-  State<TasksList> createState() => _TasksListState();
-}
-
-// class _TasksListState extends State<TasksList> {
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: widget.tasks.length,
-//       itemBuilder: (context, index) {
-//         return TaskTile(
-//           isChecked: widget.tasks[index].isDone,
-//           taskTitle: widget.tasks[index].name,
-//           checkboxCallback: (checkboxState) {
-//             setState(() {
-//               widget.tasks[index].toggleDone();
-//             });
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
-class _TasksListState extends State<TasksList> {
-
+class TasksList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -43,9 +13,7 @@ class _TasksListState extends State<TasksList> {
           isChecked: context.read<TaskData>().tasks[index].isDone,
           taskTitle: context.read<TaskData>().tasks[index].name,
           checkboxCallback: (checkboxState) {
-            setState(() {
-              context.read<TaskData>().tasks[index].toggleDone();
-            });
+            context.read<TaskData>().tasks[index].toggleDone();
           },
         );
       },
