cf3c69bb1c489c051ed919c5c689460c4bfc715b
ParkGiraffe <op5321@soongsil.ac.kr>
2022-02-07 15:32:40 +0900

    feat: todoey - Consumer()를 이용하여 provider 키워드 생략

--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/src/provider.dart';
 import 'package:todoey/model/task_data.dart';
 import 'package:todoey/widgets/task_tile.dart';
@@ -6,17 +7,39 @@ import 'package:todoey/widgets/task_tile.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          isChecked: context.read<TaskData>().tasks[index].isDone,
-          taskTitle: context.read<TaskData>().tasks[index].name,
-          checkboxCallback: (checkboxState) {
-            context.read<TaskData>().tasks[index].toggleDone();
+    return Consumer<TaskData>(
+      builder: (_, TaskData, __) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: TaskData.tasks[index].isDone,
+              taskTitle: TaskData.tasks[index].name,
+              checkboxCallback: (checkboxState) {
+                TaskData.tasks[index].toggleDone();
+              },
+            );
           },
+          itemCount: TaskData.tasks.length,
         );
-      },
-      itemCount: context.read<TaskData>().tasks.length,
+      }
     );
   }
 }
+
+// class TasksList extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemBuilder: (context, index) {
+//         return TaskTile(
+//           isChecked: context.read<TaskData>().tasks[index].isDone,
+//           taskTitle: context.read<TaskData>().tasks[index].name,
+//           checkboxCallback: (checkboxState) {
+//             // context.read<TaskData>().tasks[index].toggleDone();
+//           },
+//         );
+//       },
+//       itemCount: context.read<TaskData>().tasks.length,
+//     );
+//   }
+// }
